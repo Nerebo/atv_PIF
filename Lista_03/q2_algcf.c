56c6084f6ea7e8e8d91aa1db08481dcb7d7401cd
Andre Fonseca <106044097+Nerebo@users.noreply.github.com>
2023-09-20 10:17:38 -0300

    Update q2_algcf.c
    código finalizado

--- Lista_03/q2_algcf.c
+++ Lista_03/q2_algcf.c
@@ -1,40 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int N, K, i, j;
+int N, K, i, j, diferentes, iguais;
 char s[20];
-char pt[] = "portugues";
-char es[] = "espanhol";
-char en[] = "ingles";
-char ch[] = "chines";
+char ln[20];
 
 int main(void) {
+  
 scanf("%d", &N);
-  for(i=0; i<= N; i++){
+  for(i=0; i < N; i++){
     scanf("%d", &K);
-    if(K > 1 && K <= 20){
-    for(j=0; j<N; j++){
+
+    scanf("%s", ln);
       
-      scanf("%s \n", s);
-      
-      if(strcmp(s, pt) == 0){
-      printf("A \n");  
+      for(j=1; j < K; j++){
+      scanf("%s", s);
+      iguais++;
+      if(strcmp(s,ln) == 0){
+          }
         
-      }
-      if(strcmp(s, es) == 0){
-      printf("B \n");
-        
-      }
-      if(strcmp(s, en) == 0){
-      printf("C \n");  
-        
-      }
-      if(strcmp(s, ch) == 0){
-       printf("D \n");
-        
-        }
-      } 
+        else{
+          iguais = -1;
+          diferentes = 1;
+        memset(ln, 0, 20);
+        strcpy(ln, s);
+            }
+          }
+    
+    if(diferentes != 1 && iguais > 0){
+      printf("%s\n", ln);
     }
+    else{  
+      printf("ingles\n");
+    }
+      }
+   return 0;
   }
-  return 0;
-}
