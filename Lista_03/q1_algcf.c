83daabcbef30cf16253c357fcf83cd0c757391d3
Andre Fonseca <106044097+Nerebo@users.noreply.github.com>
2023-09-27 11:49:55 -0300

    Update q1_algcf.c

--- Lista_03/q1_algcf.c
+++ Lista_03/q1_algcf.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-char teste[50], teste1[50];
-int i;
+char teste[201];
+char alfabeto[26] = "abcdefghijklmnopqrstuvwxyz";
+int N, i, j, k, iguais = 0;
 
-int main(void) {
-
-   scanf("%[^\n]%*c", teste);
-
-  for(i=0; i < strlen(teste) ; i++){
-  teste1[i] = tolower(teste[i]);
+int main(void){
+  scanf("%d", &N);
+  for(i=0; N > i; i++){
+    char repetir[26] = {0};
+  scanf(" %200[^\n]", teste);
+  for(j=0; j < strlen(teste) ; j++){
+  teste[j] = tolower(teste[j]);
   }
-
-  printf("%s %s", teste, teste1);
+    
+    for(j = 0; j < 26; j++){
+      for(k = 0; strlen(teste) > k; k++){
+        if (alfabeto[j] == teste[k]){
+          repetir[j]++;
+        }
+      }
+    }
+    
+   for(j = 0; j < 26; j++){
+     if (0 == j){
+       iguais = repetir[j];
+     }
+     if(repetir[j] > iguais){
+       iguais = repetir[j];
+     }
+   }
+  for(j = 0; j < 26; j++){
+    if(iguais == repetir[j]){
+      printf("%c", alfabeto[j]);
+    }
+  }
+  printf("\n");
+  }
+  
   return 0;
-}
+}  
